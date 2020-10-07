dbcf38c449a5cb3d7aacbbafe47565a89aca3dfc
a <a@a.com>
2020-10-07 14:46:04 +0800

    'a'

--- TimeCapsule/Console.py
+++ TimeCapsule/Console.py
@@ -1,9 +1,31 @@
+import PyBear.Bear as Bear
 import PyBear.TimeCapsule.Core as Core
+import PyBear.Math.Cipher as Cipher
 
-def AF(Clue):
-    return input(Clue)
-Core.AskFunction = AF
+import os
+
+os.system('cls')
+
+while True:
+    Key = input('Input Password: ')
+    if Cipher.AESDecrypt('Y2VlZWM1M2MxY2I5MTQxYjczNTJlNTcwZmIxN2E1YWU=', Key) == 'Success':
+        break
+    print('Password Wrong!')
+
+print('Login Success...')
+input("Press Enter To Continue ...")
+os.system('cls')
+
+Bear.NewServer('ConsoleServer', Key, 'OWM4MjVjZmMyNWJjMzIwZmRjYjc3NzU2MTMwZjcyZGU=YTczYmZmZThmMjZhNDUzNzZkMTY5NDQ4ZGRiYzhmMzY=OGRiNjYwOThhNzYwZTRmYTdiN2E3ODZkYTJkNWVjNzI=YjA5N2VmZDU3YWJjYzNmYzI3MTIzMDJkZjA0NGE3Yjc=')
+
+def AdditionHandler(Explain):
+    return input(Explain+': ')
+Core.SetAdditionHandler(AdditionHandler)
 
 while(True):
     Input = input("Code: ")
-    Core.Cast(Input)
+    if Bear.CatchResult(Core.Cast(Input)):
+        input('Success. Press Enter To Continue...')
+    else:
+        input('Failed. Press Enter To Continue...')
+    os.system('cls')
