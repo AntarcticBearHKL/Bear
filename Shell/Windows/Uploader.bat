6320c91bb6404c06e512e7e6c080123317fa106a
Happy <antarcticbearhkl@gmail.com>
2020-12-16 22:49:46 +0800

    'a'

--- Shell/Windows/Uploader.bat
+++ Shell/Windows/Uploader.bat
@@ -14,3 +14,15 @@ cd /d"F:\Github\CppBear"
 git add .
 git commit -m 'a'
 git push
+cd /d"F:\Github\BearCompiler"
+git add .
+git commit -m 'a'
+git push
+cd /d"F:\Github\BearOS"
+git add .
+git commit -m 'a'
+git push
+cd /d"F:\Github\BearCPU"
+git add .
+git commit -m 'a'
+git push
