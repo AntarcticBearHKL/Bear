75eec2284f51462fd985e5b187694a9157ec1e81
Your Name <you@example.com>
2021-02-16 11:47:25 +0800

    a

--- program/coreSystem/Server.py
+++
@@ -1,9 +0,0 @@
-import os,sys
-
-import PyBear.Library.WebSuite as WebSuiteBear
-
-def GetHandler(Client):
-    #Client.PrintRequest()
-    Client.ReturnPageGet()
-
-WebSuiteBear.StartHttpServer(ApplicationFileLocation='F:\Github\FileBear\Application\ControlPanel\WebShell', LibraryFileLocation='F:\Github\JsBear', GetHandler=GetHandler, PostHandler=None, ParameterAnalyst=None,)
